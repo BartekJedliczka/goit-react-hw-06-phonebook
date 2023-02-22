e70cbe074b025e9305d42a2b0de05c737e37379e
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-21 19:53:29 -0500

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,28 +35,33 @@ export const ContactForm = () => {
   return (
     <>
       <form className={css.form} onSubmit={handleSubmit}>
-        <label className={css.label}>Name</label>
-        <input
-          className={css.input}
-          type="text"
-          name="name"
-          autoComplete="off"
-          placeholder="e.g. John Doe"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <label className={css.label}>Number</label>
-        <input
-          className={css.input}
-          type="tel"
-          name="number"
-          autoComplete="off"
-          placeholder="e.g. 560-432-234"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
+        <label className={css.label}>
+          Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            autoComplete="off"
+            placeholder="e.g. John Doe"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label className={css.label}>
+          Number
+          <input
+            className={css.input}
+            type="tel"
+            name="number"
+            autoComplete="off"
+            placeholder="e.g. 560-432-234"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
 
         <button className={css.button} type="submit">
           Add contact
